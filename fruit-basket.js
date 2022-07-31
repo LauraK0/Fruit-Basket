f9710dcb0501f36da1084701db33fc3447aa74cc
LauraK0 <laurakeating@live.co.uk>
2022-07-31 15:20:40 +0100

    formatting changed

--- fruit-basket.js
+++ fruit-basket.js
@@ -7,7 +7,7 @@ if (document.readyState == 'loading') {
 
 /* event listeners*/ 
 function ready() {
-  var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+  var removeCartItemButtons = document.getElementsByClassName('remove-button')
   for (var i = 0; i < removeCartItemButtons.length; i++) {
       var button = removeCartItemButtons[i]
       button.addEventListener('click', removeCartItem)
@@ -25,7 +25,7 @@ function ready() {
       button.addEventListener('click', addToCartClicked)
   }
 
-  document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+  document.getElementsByClassName('purchase-button')[0].addEventListener('click', purchaseClicked)
 }
 
 /* items purchased */ 
@@ -84,11 +84,11 @@ function addItemToCart(title, price, imageSrc) {
       <span class="cart-price cart-column">${price}</span>
       <div class="cart-quantity cart-column">
           <input class="cart-quantity-input" type="number" value="1">
-          <button class="btn btn-danger" type="button">REMOVE</button>
+          <button class="button remove-button" type="button">REMOVE</button>
       </div>`
   cartRow.innerHTML = cartRowContents
   cartItems.append(cartRow)
-  cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+  cartRow.getElementsByClassName('remove-button')[0].addEventListener('click', removeCartItem)
   cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
 }
 
@@ -101,10 +101,10 @@ function updateCartTotal() {
       var cartRow = cartRows[i]
       var priceElement = cartRow.getElementsByClassName('cart-price')[0]
       var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-      var price = parseFloat(priceElement.innerText.replace('$', ''))
+      var price = parseFloat(priceElement.innerText.replace('£', ''))
       var quantity = quantityElement.value
       total = total + (price * quantity)
   }
   total = Math.round(total * 100) / 100
-  document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+  document.getElementsByClassName('cart-total-price')[0].innerText = '£' + total
 }
